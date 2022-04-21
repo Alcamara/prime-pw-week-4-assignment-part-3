2e156669e1e3fbe612173484cc454a7a57967acf
Al Camara <Alhousseinec@gmail.com>
2022-04-21 08:39:43 -0500

    finshed 'Using Array built-in functions' stretch Goal

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,13 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+
+//Using functions in other functions part 1
 const maxItems = 5;
 
 let addItem = item => {
     
+    //Using functions in other functions part 3
     if(!isFull()) {
         basket.push(item); 
         return true
@@ -15,6 +18,7 @@ let addItem = item => {
 
 }
 
+//Using functions in other functions part 2
 function isFull(){
     let numItems = basket.length;
     return numItems >= maxItems ? true : false;
@@ -30,7 +34,7 @@ function listItems(basket){
 
 console.log('Was item added to basket?', addItem('Jam'));
 console.log('Was item added to basket?', addItem('Eggs'));
-console.log('Was item added to basket?', addItem('Bacon'));
+console.log('Was item added to basket?', addItem('Bread'));
 console.log('Was item added to basket?', addItem('Juice'));
 
 
@@ -39,11 +43,17 @@ console.log('Is the basket full?',isFull());
 console.log('Was item added to basket?', addItem('Bread'));
 
 console.log('Is the basket full?',isFull());
-console.log('Was item added to basket?', addItem('Bread'));
+console.log('Was item added to basket?', addItem('Bacon'));
 
 listItems(basket)
 
 console.log(basket);
+
+console.log('Item removed for carts is', removeItem('Bread'));
+
+console.log(basket);
+
+console.log('Item removed for carts is', removeItem('Milk'));
 
 function empty(){
     basket = [];
@@ -54,3 +64,11 @@ empty()
 console.log(basket);
 
 
+
+
+// Using Array built-in functions!
+function removeItem(item){
+    let indexLocation = basket.indexOf(item);
+    
+    return indexLocation !== -1 ? basket.splice(indexLocation,1): null;
+}
